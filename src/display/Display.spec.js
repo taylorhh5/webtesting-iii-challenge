15587ce4a8c17752849f6af364227807eb6246c6
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-09-16 19:11:58 -0400

    testing for closed class

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -7,3 +7,21 @@ test("Render the Dashboard", () => {
     render (<Display/>);
     
     })
+    test ('should match snapshot',() =>{
+        expect (render (<Display/>)).toMatchSnapshot();
+     });
+           
+    
+    test('defaults to unlocked and open', () => {
+        const { getByText } = render(<Display />);
+        getByText(/unlocked/i);
+        getByText(/open/i);
+      });
+
+  
+
+    test('when unlocked or open use the green-led class', () => {
+        const  {getByText}  = render(<Display closed={false} />);
+        getByText (/Unlocked/i);
+        toHaveClass(/green-led/i);
+    })
