967a777dcbc9f4abe5e4fa463bd7c1633fc4422b
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-09-16 17:29:55 -0400

    added a snapshot for display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,9 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Display from './Display.js'
+
+test("Render the Dashboard", () => {
+    render (<Display/>);
+    
+    })
