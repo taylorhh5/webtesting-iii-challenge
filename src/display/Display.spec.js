030d6ee59b8960786d3f26c9f81825ac7b83a46e
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-09-17 08:58:07 -0400

    added test for gate closed

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import Display from './Display.js'
 
+
 test("Render the Dashboard", () => {
     render (<Display/>);
     
@@ -21,7 +22,23 @@ test("Render the Dashboard", () => {
   
 
     test('when unlocked or open use the green-led class', () => {
+        const  {getByText}  = render(<Display locked={false} />);
+        const notLocked = getByText (/Unlocked/i);
+        expect (notLocked).toBeTruthy;
+    })
+
+
+
+    
+    test('displays Locked if the locked prop is true', () => {
+        const  {getByText}  = render(<Display locked={true} />);
+        const isLocked = getByText (/locked/i);
+        expect (isLocked).toBeTruthy;
+    })
+
+
+    test('when closed use the red-led class', () => {
         const  {getByText}  = render(<Display closed={false} />);
-        getByText (/Unlocked/i);
-        toHaveClass(/green-led/i);
+        const isClosed = getByText (/open/i);
+        expect(isClosed.classList.contains('green-led '))
     })
